36e6650e64f3b9e7f0959dcb5d8d1bb46fd5fdad
Caleb Weeks <me@sethcalebweeks.com>
2023-01-12 09:43:19 -0500

    acronym with no regex

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,19 +1,14 @@
 (ns acronym 
   (:require [clojure.string :as str]))
 
-(defn acronym-word
-  [word]
-  (let [upper (filter #(not (Character/isLowerCase %)) word)]
-    (if (= (count upper) (count word))
-     (first word)
-     (apply str upper))))
-
-(defn capitalize-first
-  [word]
-  (let [[first & rest] word] (apply str (cons (str/upper-case first) rest))))
+(defn keep-capitals [acc [fst snd]]
+  (cond
+    (and (Character/isUpperCase fst) (not (Character/isUpperCase snd))) (str acc fst) 
+    (and (not (Character/isLetter fst)) (Character/isLowerCase snd)) (str acc (str/upper-case snd))
+    :else acc))
 
 (defn acronym
   "Converts phrase to its acronym."
   [phrase]
-  (apply str (map #(acronym-word (capitalize-first %)) (filter #(not= % "") (str/split phrase #"[ ,-]"))))
+  (reduce keep-capitals "" (partition 2 1 phrase))
   )
