7288dc74ef7b07faa9dfed714a3b0de7f726e6ee
Caleb Weeks <me@sethcalebweeks.com>
2023-01-19 00:32:10 -0500

    all-your-base done

--- clojure/all-your-base/src/all_your_base.clj
+++ clojure/all-your-base/src/all_your_base.clj
@@ -1,5 +1,29 @@
 (ns all-your-base)
 
-(defn convert [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+(defn factors [num base]
+  (reverse (for [digit (range)
+                 :let [factor (exp base digit)]
+                 :while (<= factor num)] factor)))
+
+(defn digits [num factors]
+  (cond
+    (empty? factors) []
+    :else (let [digit (unchecked-divide-int num (first factors))
+                remainder (mod num (first factors))]
+            (cons digit (digits remainder (rest factors))))))
+
+(defn dec-to-base [num base]
+  (digits num (factors num base)))
+
+(defn base-to-dec [base num] (reduce #(+ %2 (* %1 base)) 0 num))
+
+(defn convert [a num b]
+  (cond
+    (or (< a 2) (< b 2)) nil
+    (empty? num) []
+    (some #(or (>= % a) (< % 0)) num) nil
+    (every? #(= % 0) num) [0] 
+    :else (dec-to-base (base-to-dec a num) b)))
