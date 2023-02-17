8789776e3633b016dccc5f6bec944cf90e1408e4
Caleb Weeks <me@sethcalebweeks.com>
2023-02-16 22:15:52 -0500

    raindrops

---
+++ clojure/raindrops/src/raindrops.clj
@@ -0,0 +1,24 @@
+(ns raindrops
+  (:require [clojure.string :as str]))
+
+(defn defsound [sound factor]
+  (let [fn-name (symbol sound)] 
+    (intern *ns* fn-name  
+     (fn [n] (if (= 0 (mod n factor)) (str/capitalize sound) "")))))
+
+(defsound "pling" 3)
+(defsound "plang" 5)
+(defsound "plong" 7)
+
+(defn convert [n]
+  (let [factors (str/join (map #(% n) [pling plang plong]))]
+    (if (empty? factors) (str n) factors)))
+
+
+(comment
+  (str/join (map #(% 21) [pling plang plong]))
+  (defsound "pling" 3)
+  (symbol "pling")
+  (create-function "testing")
+  (testing)
+  (pling 9))
