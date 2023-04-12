da823554db98704dab3a1284e0ee2416e9c59b0e
Caleb Weeks <me@sethcalebweeks.com>
2023-04-12 07:06:01 -0400

    ocaml bob complete

--- ocaml/bob/bob.ml
+++ ocaml/bob/bob.ml
@@ -1,13 +1,15 @@
-let is_question s =
-  String.trim s
-  |> String.to_seq
-  |> List.of_seq
-  |> List.rev
-  |> List.hd
-  |> (=) '?'
+let is_question = String.ends_with ~suffix:"?"
 
-let is_empty s = String.trim s = ""
-let response_for = function
+let is_yell s = 
+  let upper = String.uppercase_ascii s in
+  let is_letter c = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') in
+  let count = s |> String.to_seq |> Seq.filter is_letter |> Seq.length in
+    s = upper && count > 0
+
+let response_for s = 
+  match String.trim s with
+  | "" -> "Fine. Be that way!"
+  | s when is_yell s && is_question s -> "Calm down, I know what I'm doing!"
   | s when is_question s -> "Sure."
-  | s when is_empty s -> "Fine. Be that way!"
+  | s when is_yell s -> "Whoa, chill out!"
   | _ -> "Whatever."
